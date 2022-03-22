8773b72b4367a4d3db18dc3003e567f608dbd163
Perkq <artsynek@gmail.com>
2022-03-23 00:00:59 +0300

    +footer
    Добавил нормальный футер(цвет бэкграунда, форматирование текста)

--- index.php
+++ index.php
@@ -874,17 +874,9 @@ if(isset($_POST['bsearch'])) {
 (function(){ var widget_id = 'o0KDbaO5fS';var d=document;var w=window;function l(){
 var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);}if(d.readyState=='complete'){l();}else{if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();</script>
 <!-- {/literal} END JIVOSITE CODE -->
-    <footer>
- <font size="2">
- <p align="center">
- И.П. Нестеренко Е.А. УНП 791041642 от 28.04.2016 выдано Администрацией Октябрьского района г.Могилёва
- </p>
-
-<p align="center">  
-i-m.by	© 2017
-  </p>
-  </font>
- 
+ <footer class= "bottom">
+	<p class = "p_footer">И.П. Нестеренко Е.А. УНП 791041642 от 28.04.2016 выдано Администрацией Октябрьского района г.Могилёва</p>
+	<p class = "p_footer">  i-m.by	© 2017</p>
   </footer>
 </body>
 </html>
